5af407846e3643b35c20da781ba5b143a8dcbf2c
Frrama <arjunafadli@gmail.com>
2025-02-19 19:51:34 +0700

    update

--- resources/views/staff/dashboard.blade.php
+++ resources/views/staff/dashboard.blade.php
@@ -64,6 +64,89 @@
             setInterval(updateClock, 1000);
             updateClock(); // Run once immediately to display the time right away
         </script>
+
+
+        <div class="row">
+            <div class="col-lg-3 mb-4">
+                <div class="card"
+                    style="background: url('public/assets/img/illustrations/content1.jpg') center/cover no-repeat;">
+                    <div class="card-body">
+                        <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="avatar flex-shrink-0">
+                                <img src="../public/assets/img/icons/unicons/chart-success2.png" alt="Credit Card"
+                                    class="rounded" />
+                            </div>
+                        </div>
+                        <?php
+                        $numberOfUsers = \App\Models\User::count();
+                        ?>
+                        <span>Jumlah</span>
+                        <br><span><b>Pengguna</b></span>
+                        <br>&nbsp;
+                        <h3 class="card-title text-nowrap mb-1">{{ $numberOfUsers }}</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 mb-4">
+                <div class="card"
+                    style="background: url('public/assets/img/illustrations/content1.jpg') center/cover no-repeat;">
+                    <div class="card-body">
+                        <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="avatar flex-shrink-0">
+                                <img src="../public/assets/img/icons/unicons/chart-success1.png" alt="chart success"
+                                    class="rounded" />
+                            </div>
+                        </div>
+                        <?php
+                        $inputs = \App\Models\Instansi::all()->count();
+                        ?>
+                        <span>Jumlah</span>
+                        <br><span><b>Instansi Kerjasama</b></span>
+                        <br>&nbsp;
+                        <h3 class="card-title text-nowrap mb-1">{{ $instansis }}</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 mb-4">
+                <div class="card"
+                    style="background: url('public/assets/img/illustrations/content1.jpg') center/cover no-repeat;">
+                    <div class="card-body">
+                        <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="avatar flex-shrink-0">
+                                <img src="../public/assets/img/icons/unicons/chart-success1.png" alt="Credit Card"
+                                    class="rounded" />
+                            </div>
+                        </div>
+                        <?php
+                        $numberOfUsers = \App\Models\Sekolah::count();
+                        ?>
+                        <span>Jumlah</span>
+                        <br><span><b>Sekolah Kerjasama</b></span>
+                        <br>&nbsp;
+                        <h3 class="card-title text-nowrap mb-1">{{ $sekolahs }}</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 mb-4">
+                <div class="card"
+                    style="background: url('public/assets/img/illustrations/content1.jpg') center/cover no-repeat;">
+                    <div class="card-body">
+                        <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="avatar flex-shrink-0">
+                                <img src="../public/assets/img/icons/unicons/chart-success1.png" alt="Credit Card"
+                                    class="rounded" />
+                            </div>
+                        </div>
+                        <?php
+                        $numberOfUsers = \App\Models\Unit_Penempatan::count(); // Correct casing
+                        ?>
+                        <span>Jumlah</span>
+                        <br><span><b>Mentor</b></span>
+                        <br>&nbsp;
+                        <h3 class="card-title text-nowrap mb-1">{{ $totalUnitPenempatans }}</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
