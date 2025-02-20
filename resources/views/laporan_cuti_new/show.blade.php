e2f5ebb78a52dccb463fb4a826b69c6eb6e50ffb
Frrama <arjunafadli@gmail.com>
2025-02-20 19:53:24 +0700

    update

--- resources/views/laporan_cuti_new/show.blade.php
+++ resources/views/laporan_cuti_new/show.blade.php
@@ -21,53 +21,53 @@
                         <hr>
                         <div class="mb-3">
                             <strong>Nama Karyawan:</strong>
-                            <p>{{ $laporanCuti->karyawan->name_lengkap }}</p>
+                            <p>{{ $laporanCutis->karyawan->name_lengkap }}</p>
                         </div>
                         <div class="mb-3">
                             <strong>Divisi:</strong>
-                            <p>{{ $laporanCuti->karyawan->divisi ?? 'Tidak tersedia' }}</p>
+                            <p>{{ $laporanCutis->karyawan->divisi ?? 'Tidak tersedia' }}</p>
                         </div>
                         <div class="mb-3">
                             <strong>Jenis Cuti:</strong>
-                            <p>{{ $laporanCuti->jenis_Cuti->nama_jenis_cuti }}</p>
+                            <p>{{ $laporanCutis->jenis_Cuti->nama_jenis_cuti }}</p>
                         </div>
                         <div class="mb-3">
                             <strong>Tanggal Pengajuan:</strong>
-                            <p>{{ $laporanCuti->tanggal_pengajuan->format('d-m-Y') }}</p>
+                            <p>{{ $laporanCutis->tanggal_pengajuan->format('d-m-Y') }}</p>
                         </div>
                         <div class="mb-3">
                             <strong>Tanggal Mulai:</strong>
-                            <p>{{ $laporanCuti->tanggal_mulai->format('d-m-Y') }}</p>
+                            <p>{{ $laporanCutis->tanggal_mulai->format('d-m-Y') }}</p>
                         </div>
                         <div class="mb-3">
                             <strong>Tanggal Selesai:</strong>
-                            <p>{{ $laporanCuti->tanggal_selesai->format('d-m-Y') }}</p>
+                            <p>{{ $laporanCutis->tanggal_selesai->format('d-m-Y') }}</p>
                         </div>
                         <div class="mb-3">
                             <strong>Alasan:</strong>
-                            <p>{{ $laporanCuti->alasan }}</p>
+                            <p>{{ $laporanCutis->alasan }}</p>
                         </div>
                         <div class="mb-3">
                             <strong>Status Persetujuan Direktur:</strong>
-                            <p>{{ $laporanCuti->approved_by_director }}</p>
+                            <p>{{ $laporanCutis->approved_by_director }}</p>
                         </div>
                         <div class="mb-3">
                             <strong>Status Persetujuan Manajer:</strong>
-                            <p>{{ $laporanCuti->approved_by_manager }}</p>
+                            <p>{{ $laporanCutis->approved_by_manager }}</p>
                         </div>
                         <div class="mb-3">
                             <strong>Catatan:</strong>
-                            <p>{{ $laporanCuti->catatan ?? 'Tidak ada catatan' }}</p>
+                            <p>{{ $laporanCutis->catatan ?? 'Tidak ada catatan' }}</p>
                         </div>
                         <div class="mb-3">
                             <strong>Aksi:</strong>
                                 <a href="{{ route('laporan_cuti_new.index') }}" class="btn btn-warning">Kembali</a>
-                                <a href="{{ route('laporan_cuti_new.edit', $laporanCuti->id_surat) }}" class="btn btn-info">Edit</a>
+                                <a href="{{ route('laporan_cuti_new.edit', $laporanCutis->id) }}" class="btn btn-info">Edit</a>
                                 @if (user()->karyawan->jabatan == '15' || Auth::user()->role_as == '1')
-                                    <a href="{{ route('laporan_cuti_new.validasi', $laporanCuti->id_surat) }}" class="btn btn-success">Format ASN</a>   
+                                    <a href="{{ route('laporan_cuti_new.validasi', $laporanCutis->id) }}" class="btn btn-success">Format ASN</a>   
                                 @endif
                                 @if (Auth::user()->karyawan->jabatan == '14' || Auth::user()->role_as == '1')
-                                    <a href="{{ route('laporan_cuti_new.validasi', $laporanCuti->id_surat) }}" class="btn btn-success">Format ASN</a>   
+                                    <a href="{{ route('laporan_cuti_new.validasi', $laporanCutis->id) }}" class="btn btn-success">Format ASN</a>   
                                 @endif
                         </div>
                     </div>
