deec99b9bb5d62ba3811d76abf07f7934bb890b5
Frrama <arjunafadli@gmail.com>
2025-02-21 08:48:28 +0700

    update

--- app/Http/Controllers/LaporanCutiNewController.php
+++ app/Http/Controllers/LaporanCutiNewController.php
@@ -67,7 +67,7 @@ class LaporanCutiNewController extends Controller
     public function show($id)
     {
         // Mencari laporan cuti berdasarkan ID
-        $laporanCutis = LaporanCutiNew::with(['karyawan', 'Jenis_cuti'])->findOrFail($id);
+        $laporanCutis = LaporanCutiNew::with(['karyawan', 'jeniscuti'])->findOrFail($id);
 
         // Mengembalikan view dengan data laporan cuti
         return view('laporan_cuti_new.show', compact('laporanCutis'));
