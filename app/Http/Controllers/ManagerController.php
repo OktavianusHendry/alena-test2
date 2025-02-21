71e22c6408e54a07531339be072d22911cc255c4
Frrama <arjunafadli@gmail.com>
2025-02-21 10:39:59 +0700

    update

---
+++ app/Http/Controllers/ManagerController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Instansi;
+use App\Models\User;
+use App\Models\Sekolah;
+use App\Models\Kategori;
+use App\Models\Sub_Kategori;
+use App\Models\Unit_Penempatan;
+
+class StaffController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth','verified']);
+    }
+    
+    public function index()
+    {
+        // Hitung jumlah data untuk statistik
+        $instansis = Instansi::count();
+        $users = User::count();
+        $sekolahs = Sekolah::count();
+        $totalUnitPenempatans = Unit_Penempatan::count();
+
+        // Kirimkan data ke view
+        return view('staff.dashboard', compact('instansis', 'users', 'sekolahs', 'totalUnitPenempatans',));
+    }
+}
