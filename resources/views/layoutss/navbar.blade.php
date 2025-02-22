0899fedae66d432edc29e89fed1fe91f06edc3a6
Frrama <arjunafadli@gmail.com>
2025-02-22 16:51:32 +0700

    update

--- resources/views/layoutss/navbar.blade.php
+++ resources/views/layoutss/navbar.blade.php
@@ -71,7 +71,7 @@
                             @endif
                         </a>
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            @forelse (auth()->user()->notifications as $notification)
+                            @forelse (optional(auth()->user())->notifications as $notification)
                                 <div class="d-flex justify-content-between align-items-center">
                                     <a class="dropdown-item" href="#">
                                         {{ $notification->data['message'] }}
