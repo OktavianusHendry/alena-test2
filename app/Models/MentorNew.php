0e6f5bc76aa38c0c57983973760fc61aaa146157
Frrama <arjunafadli@gmail.com>
2025-02-22 16:23:37 +0700

    update

--- app/Models/MentorNew.php
+++ app/Models/MentorNew.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class Mentor extends Authenticatable
+class MentorNew extends Authenticatable
 {
     use Notifiable;
 
